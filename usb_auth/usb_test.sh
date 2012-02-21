365363a1dc0e508fde65962244da47ffd6f73696
boyan <boyan.sheytanov@asteasolutions.com>
2012-02-21 15:04:42 +0200

    Removing the Node.js server. The script now directly calls the preferences server

--- usb_auth/usb_test.sh
+++ usb_auth/usb_test.sh
@@ -9,8 +9,11 @@ if [ $1 -eq 1 ]; then
 	done
 	loc=`mount | grep $2 |cut -d " " -f 3` # The location where the USB disk drive is mounted.
 	token=`cat $loc/token.txt` # The token that identifies the user.
-	curl http://localhost:3000/user?action=login\&device=$2\&token=$token # A call to the Node.js server
+	echo "User logged in on device $2 with token ${token}." >> log.txt
+	curl http://localhost:3000/user/$token # A call to the preferences server
+	# curl http://localhost:3000/user/$token/login # A call to the Flow Manager
 else
 	# USB disk drive is removed.
-	curl http://localhost:3000/user?action=logout\&device=$2
+	echo "User logged out from device $2." >> log.txt
+	# curl http://localhost:3000/user/$token/logout # A call to the Flow Manager
 fi
