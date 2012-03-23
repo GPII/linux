29cb3617f17e1ef92ba92a3487d7d10990262ffd
Yura Zenevich <yura.zenevich@gmail.com>
2012-03-23 11:24:22 -0400

    Added initial gpii.linux module

---
+++ node_module/linux/src/gpii.linux.js
@@ -0,0 +1,10 @@
+(funciton () {
+
+    var fluid = require("infusion");
+    var gpii = fluid.registerNamespace("gpii");
+
+    gpii.launch.gsettings = function () {
+        fluid.log("HAHA, SUCCESS, HAHA");
+    };
+
+})();
