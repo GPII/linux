2e9ebfa7743640d4103be057bd204acc4cfd9a0b
Colin Clark <colinbdclark@gmail.com>
2012-04-02 11:06:58 -0400

    Updated installation and USB Listener scripts to remove hard-coded paths. Added correct calls to /login and /logout in handleUserDeviceEvent.sh. Updated sample user token to correspond with our test preferences profile.

--- usbDriveListener/bin/handleUserDeviceEvent.sh
+++ usbDriveListener/bin/handleUserDeviceEvent.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+usersFilePath="/var/lib/gpii/users.txt"
+
 if [ $1 -eq 1 ]; then
 	# USB disk drive is added.
 	retries=0
@@ -9,13 +11,14 @@ if [ $1 -eq 1 ]; then
 	done
 
 	mountLocation=`mount | grep $2 |cut -d " " -f 3`
-	token=`cat $mountLocation/token.txt`
+	token=`cat $mountLocation/.gpii-user-token.txt`
 	echo "User logged in on device $2 with token ${token}." >> log.txt
-	echo $2:$token >> users.txt						# Keep the location and token in a users file.
-	curl http://localhost:3000/user/$token
+	echo $2:$token >> "$usersFilePath"						# Keep the location and token in a users file.
+	curl http://localhost:3000/user/$token/login
 else
 	# USB disk drive is removed.
-	token=`grep $2 < users.txt | cut -d ":" -f 2`
-	sed -ie "\|^$2|d" users.txt 						# Remove entry from the users file
+	curl http://localhost:3000/user/$token/logout
+	token=`grep $2 < "$usersFilePath" | cut -d ":" -f 2`
+	sed -ie "\|^$2|d" "$usersFilePath" 						# Remove entry from the users file
 	echo "User logged out from device $2 with token ${token}." >> log.txt
 fi
