1672e366eb0b2b43b21404ea6c31d1d3eeae9c19
Colin Clark <colinbdclark@gmail.com>
2012-04-02 11:23:21 -0400

    Updated license headers in all code files. Updated USB Listener instructions in README.

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# GPII Linux Build Script
+#
+# Copyright 2012 OCAD University
+#
+# Licensed under the New BSD license. You may not use this file except in
+# compliance with this License.
+#
+# You may obtain a copy of the License at
+# https://github.com/gpii/universal/LICENSE.txt
+
 node_modules="../node_modules"
 universal="../node_modules/universal"
 repoURL="git://github.com/GPII/universal.git"
