729ccc90c5c54853ff13a65d17caacd980311882
Colin Clark <colinbdclark@gmail.com>
2012-04-02 11:06:58 -0400

    Updated installation and USB Listener scripts to remove hard-coded paths. Added correct calls to /login and /logout in handleUserDeviceEvent.sh. Updated sample user token to correspond with our test preferences profile.

--- build.sh
+++ build.sh
@@ -3,9 +3,12 @@
 node_modules="../node_modules"
 universal="../node_modules/universal"
 repoURL="git://github.com/GPII/universal.git"
+usbListenerDir="./usbDriveListener"
+gpiiInstallDir="/usr/local/gpii"
+gpiiStateDir="/var/lib/gpii"
 
 # Clone the necessary GPII framework dependencies from Git.
-
+# TODO: Deal with cut and pastage for directory creation logic.
 if [ -d $node_modules ]; then
 	echo "$node_modules already exists"
 else
@@ -24,3 +27,27 @@ fi
 # Compile the GSettings C++ Bridge
 cd node_modules/gsettingsBridge/nodegsettings
 node-waf configure build
+
+
+# Create standard directory structure for GPII.
+# Note: everything below here must be run as root, since we're installing ourselves centrally.
+if [ -d $gpiiInstallDir ]; then
+	echo "$gpiiInstallDir already exists"
+else
+    echo "$gpiiInstallDir does not exist"
+    echo "creating $gpiiInstallDir"
+	mkdir -p "$gpiiInstallDir"
+fi
+
+if [ -d $gpiiStateDir ]; then
+	echo "$gpiiStateDir already exists"
+else
+    echo "$gpiiStateDir does not exist"
+    echo "creating $gpiiStateDir"
+	mkdir -p "$gpiiStateDir"
+fi
+
+# Install the USB Drive User Listener
+# TODO: We should install the entire GPII in /usr/local/gpii, not just the USB Listener
+cp -r "$usbListenerDir/bin" "$gpiiInstallDir/bin"
+cp "$usbListenerDir/80-local.rules" /etc/udev/rules.d/
--- usbDriveListener/bin/trigger.sh
+++ usbDriveListener/bin/trigger.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # This is a workaround for correct udev script execution.
 # There should be a way to avoid using a intermediate file.
-cd /home/boyan/repos/gpiistack/usb_auth
-./usb_test.sh $1 $2 & exit
+
+gpiiBinPath="/usr/local/gpii/bin"
+
+"$gpiiBinPath/handleUserDeviceEvent.sh" $1 $2 & exit
