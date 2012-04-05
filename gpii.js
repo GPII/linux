327fb212e1ced3da0aa7945a07d065fc1afef7de
kaspermarkus <kasper2@markus.dk>
2012-04-06 01:08:05 +0200

    Implemented the gsettingsLaunchHandler. Have not yet written tests for it yet (requires gsettings bridge). The return payloads for launchHandlers are not described on the wiki in detail. Currently it returns { statusCode: XXX, data: {...} } - with statusCode=200 and no data when succeeding. If failing, some other statusCode (from gsettingsBridge) is returned along with data being what was returned from the gsettingsBridge

--- gpii.js
+++ gpii.js
@@ -2,5 +2,6 @@ var fluid = require("universal"),
     gpii = fluid.registerNamespace("gpii");
 
 fluid.require("gsettingsBridge", require);
+fluid.require("gsettingsLaunchHandler", require);
 
-gpii.flowManager();
+gpii.flowManager(); 
