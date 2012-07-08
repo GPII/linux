fdbe83abeee942d956fc8e2b18b7f1c1d2684903
Kasper Markus <kasper2@markus.dk>
2012-07-08 09:42:07 +0200

    Continued work on integration testing. Currently there is an issue with the synchronicity - meaning that the logout call will be issued right after the login call

--- tests/integrationTesting.js
+++ tests/integrationTesting.js
@@ -41,40 +41,6 @@ var integrationTestsJSON = {
     //         ]
     //     }
     // }, 
-    "MikelVargas": {
-        environments: {
-            "gnome": [
-                {
-                    "type": "gpii.gsettings.get", 
-                    "data": [
-                        {
-                            "options": {
-                                "schema": "org.gnome.desktop.a11y.applications"
-                            },
-                            "settings": {
-                                "screen-keyboard-enabled": true
-                            }
-                        },
-                        {
-                            "options": {
-                                "schema": "org.gnome.desktop.a11y.keyboard"
-                            },
-                            "settings": {
-                                "stickykeys-enable": true,
-                                "slowkeys-enable": true,
-                                "slowkeys-delay": 400,
-                                "bouncekeys-enable": true,
-                                "bouncekeys-delay": 200,
-                                "mousekeys-enable": true,
-                                "mousekeys-init-delay": 120,
-                                "mousekeys-max-speed": 850,
-                                "mousekeys-accel-time": 800
-                            }
-                        }
-                    ]
-                }
-            ]
-        }
     // },   
     // "carla": {
     //     environments: {
@@ -99,7 +65,70 @@ var integrationTestsJSON = {
     //             }
     //         ]
     //     }
-    }
+    "MikelVargas": {
+        "initialState": [
+            {
+                "type": "gpii.gsettings.get",
+                "data": [
+                    {
+                        "options": {
+                            "schema": "org.gnome.desktop.a11y.applications"
+                        },
+                        "settings": {
+                            "screen-keyboard-enabled": false
+                        }
+                    },
+                    {
+                        "options": {
+                            "schema": "org.gnome.desktop.a11y.keyboard"
+                        },
+                        "settings": {
+                            "stickykeys-enable": false,
+                            "slowkeys-enable": false,
+                            "slowkeys-delay": 100,
+                            "bouncekeys-enable": false,
+                            "bouncekeys-delay": 100,
+                            "mousekeys-enable": false,
+                            "mousekeys-init-delay": 100,
+                            "mousekeys-max-speed": 100,
+                            "mousekeys-accel-time": 100
+                        }
+                    }
+                ]
+            }
+        ],
+        "loggedInState": [
+            {
+                "type": "gpii.gsettings.get", 
+                "data": [
+                    {
+                        "options": {
+                            "schema": "org.gnome.desktop.a11y.applications"
+                        },
+                        "settings": {
+                            "screen-keyboard-enabled": true
+                        }
+                    },
+                    {
+                        "options": {
+                            "schema": "org.gnome.desktop.a11y.keyboard"
+                        },
+                        "settings": {
+                            "stickykeys-enable": true,
+                            "slowkeys-enable": true,
+                            "slowkeys-delay": 400,
+                            "bouncekeys-enable": true,
+                            "bouncekeys-delay": 200,
+                            "mousekeys-enable": true,
+                            "mousekeys-init-delay": 120,
+                            "mousekeys-max-speed": 850,
+                            "mousekeys-accel-time": 800
+                        }
+                    }
+                ]
+            }
+        ]
+    }   
 };
 
 (function () {
@@ -115,36 +144,57 @@ var integrationTestsJSON = {
 
     var integrationTester = gpii.tests.testEnvironment();
 
-    var originalValues=null;
-    
     /*
-    * Save the original values - from before anything is changed. We'll expect these to
-    * be set again on logout. Values will be saved in same structure as the test payloads.
+    * Sets the settings given in the json paramater. The content of the json passed
+    * is the values to set in a format similar to the content of 'initialState'
     */
-    var getOriginalValues = function (fulljson) {
-        var origValues = fluid.copy(fulljson);
-        fluid.each(origValues, function (json, token) {
-            origValues[token].environments.gnome = fluid.transform(origValues[token].environments.gnome, function (testBlock, textIndex) {
-                var args = {};
-                args[token] = testBlock.data;
-                var response = fluid.invokeGlobalFunction(testBlock.type, [args]);
-                return {
-                    type: testBlock.type,
-                    data: response[token]
-                };
-            });
+    var setSettings = function (json) {
+        //go through each of the settings
+        fluid.each(json, function (handlerBlock, handlerIndex) {
+            var args = {};
+            args["setting"] = handlerBlock.data;
+            var setter = handlerBlock.type.substr(0, handlerBlock.type.indexOf(".get"))+".set";
+            var response = fluid.invokeGlobalFunction(setter, [args]);
         });
+    };
 
-        return origValues;
+    /*
+    * Checks the settings given in the json paramater. The content of the json passed
+    * should contain the expected value, and they should be in the format of the contant
+    * of 'initialState'
+    */
+    var checkSettings = function (expected) {
+        //go through each of the settings
+        fluid.each(expected, function (handlerBlock, handlerIndex) {
+            //first get the settings from the system
+            var args = {};
+            args["checking"] = handlerBlock.data;
+            var response = fluid.invokeGlobalFunction(handlerBlock.type, [args]);
+            //check that these corresponds to the one we anted to set:
+            fluid.each(handlerBlock.data, function (solutionBlock, solutionIndex) {
+                //check each setting:
+                fluid.each(solutionBlock.settings, function (expectedValue, settingKey) {
+                    var responseValue = response["checking"][solutionIndex].settings[settingKey];
+                    jqUnit.assertEquals("Check setting "+settingKey, expectedValue, responseValue);
+                });
+            });
+        })
     };
 
     var addTests = function () {
         gpii.flowManager();
 
-        //save original values:
-        originalValues = getOriginalValues(integrationTestsJSON);    
-
         fluid.each(integrationTestsJSON, function (json, token) {
+            //Setup and check an initial known state:
+            integrationTester.asyncTest("Set up initial state", function() {
+                setSettings(json.initialState);
+                checkSettings(json.initialState);
+                setTimeout(function() {
+                    jqUnit.start();
+                }, 10);
+            });
+
+            //test login with token
             integrationTester.asyncTest("Test "+token+" Login", function () {
                 http.get({
                     host: "localhost",
@@ -168,30 +218,16 @@ var integrationTestsJSON = {
                     response.on("end", function() {
                         fluid.log("Connection to server ended");
                         jqUnit.assertNotEquals("Successful login message returned", data.indexOf("User was successfully logged in."), -1);
-                        //After successful login, get settings and check that they're as expected.
-                        fluid.each(json.environments.gnome, function (testBlock, textIndex) {
-                            var args = {};
-                            args[token] = testBlock.data;
-                            //wait one second to ensure that the settings have propagated
-                            setTimeout(function() {
-                                //call the settingshandler to get the settings
-                                var changedSettings = fluid.invokeGlobalFunction(testBlock.type, [args]);
-                                //go through each of the settings to compare them:
-                                fluid.each(changedSettings[token], function (arrayEntry, arrayInd) {
-                                    //check each setting:
-                                    fluid.each(arrayEntry.settings, function (settingValue, settingKey) {
-                                        var expectedValue = testBlock.data[arrayInd].settings[settingKey];
-                                        jqUnit.assertEquals("Check setting "+settingKey, expectedValue, settingValue);
-                                    });
-                                });
-                                jqUnit.start(); 
-                            }, 1000);
-                        });
+                        setTimeout(function() {
+                            checkSettings(json.loggedInState);
+                            jqUnit.start();
+                        }, 5000);
                     });
                 }).on('error', function(err) {
                     fluid.log("Got error: " + err.message);
                     jqUnit.start();
                 });
+                jqUnit.stop();
             });
 
             integrationTester.asyncTest("Test "+token+" logout", function () {
@@ -207,7 +243,7 @@ var integrationTestsJSON = {
                     });
                     response.on("close", function(err) {
                         if (err) {
-                            jqUnit.assertFalse("Got an error on login:" + err.message, true);
+                            jqUnit.assertFalse("Got an error on logout:" + err.message, true);
                             jqUnit.start();
                         }
                         fluid.log("Connection to the server was closed");
@@ -216,25 +252,10 @@ var integrationTestsJSON = {
                         fluid.log("Logout connection to server ended");
                         jqUnit.assertNotEquals("Successful logout message returned", data.indexOf("successfully logged out."), -1);
                         //After successful logout, get settings and check that they have been properly reset
-                        //fluid.log("ORIG VALS "+JSON.stringify(originalValues));
-                        fluid.each(originalValues[token].environments.gnome, function (testBlock, textIndex) {
-                            var args = {};
-                            args[token] = testBlock.data;
-                            //wait one second to ensure that the settings have propagated
-                            setTimeout(function() {
-                                //call the settingshandler to get the settings
-                                var changedSettings = fluid.invokeGlobalFunction(testBlock.type, [args]);
-                                //go through each of the settings to compare them:
-                                fluid.each(changedSettings[token], function (arrayEntry, arrayInd) {
-                                    //check each setting:
-                                    fluid.each(arrayEntry.settings, function (settingValue, settingKey) {
-                                        var expectedValue = testBlock.data[arrayInd].settings[settingKey];
-                                        jqUnit.assertEquals("Check setting "+settingKey, expectedValue, settingValue);
-                                    });
-                                });
-                                jqUnit.start(); 
-                            }, 1000);
-                        });
+                        setTimeout(function () {
+                            checkSettings(json.initialState);
+                            jqUnit.start();
+                        }, 5000);
                     });
                 }).on('error', function(err) {
                     fluid.log("Got error: " + err.message);
