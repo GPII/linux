b7720f56e8ccaf95f311f4a2688260535bf7a3ab
kaspermarkus <kasper2@markus.dk>
2012-11-16 02:37:02 +0100

    GPII-50: removed unused global __dirname declaration

--- gpii.js
+++ gpii.js
@@ -10,8 +10,6 @@ You may obtain a copy of the License at
 https://github.com/gpii/universal/LICENSE.txt
 */
 
-/*global __dirname */
-
 var fluid = require("universal"),
     gpii = fluid.registerNamespace("gpii");
 
