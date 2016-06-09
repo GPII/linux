6f072c1f02bdc5184348a19a54ba102a399a74a8
Avtar Gill <avtargill@gmail.com>
2016-06-09 10:21:51 -0400

    GPII-1833: Sets user defaults for IDI Ansible roles

--- provisioning/install.sh
+++ provisioning/install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
-# This wrapper script bootstraps an environment to the point from where Ansible roles can be used
-# to set up the GPII Linux Framework. 
+# This wrapper script bootstraps an environment to the point where Ansible roles can be used to set
+# up the GPII Linux Framework.
 
 set -e
 
 dnf -y install ansible python-dnf
 
 # Configure Ansible for local, non-SSH use only.
-sed -i 's/^transport.*/transport = local/g' /etc/ansible/ansible.cfg
+sed -i 's/^#transport.*/transport = local/g' /etc/ansible/ansible.cfg
 sed -i 's/^#host_key_checking.*$/host_key_checking\ \=\ False/g' /etc/ansible/ansible.cfg
 cat <<-EOF > /etc/ansible/hosts
 [local]
@@ -27,6 +27,16 @@ gpgcheck=1
 gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
 EOF
 
-# Deploy the GPII Framework.
 ansible-galaxy install -fr requirements.yml
-ansible-playbook playbook.yml --tags="install,configure"
+
+# Set up the GPII Framework and get the user's account/group name, shell, and home directory in the
+# process. This information is required to run provisioning commands as the user and ensure the
+# account exists before attempting to do this. The GPII_FRAMEWORK_DIR environment variable is used
+# to figure out which directory commands should be run in.
+ansible-playbook playbook.yml --extra-vars \
+"nodejs_app_username=$(logname) \
+nodejs_app_groupname=$(id -ng $(logname)) \
+nodejs_app_user_shell=$(getent passwd $(logname) | cut -d: -f7) \
+nodejs_app_home_dir=$(getent passwd $(logname) | cut -d: -f6) \
+nodejs_app_install_dir=$GPII_FRAMEWORK_DIR" \
+--tags="install,configure"
