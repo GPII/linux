868ebe3d7f5fb5fc6e93bbe335b268b6a17d2485
Tony Atkins <tony@raisingthefloor.org>
2017-10-12 13:50:42 +0200

    GPII-2515: Converted gsettings tests to use jqUnit.

--- tests/UnitTests.js
+++ tests/UnitTests.js
@@ -1,6 +1,7 @@
 // Rollup to run all javascript-only unit tests.
 "use strict";
 require("../gpii/node_modules/alsa/test/alsaSettingsHandlerTests.js");
+require("../gpii/node_modules/gsettingsBridge/tests/gsettingsTests");
 require("../gpii/node_modules/orca/test/orcaSettingsHandlerTests.js");
 require("../gpii/node_modules/packagekit/test/all-tests.js");
 require("../gpii/node_modules/packagekit/nodepackagekit/nodepackagekit_test");
